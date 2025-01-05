d5037abb0c6aebc722074e3915774e9812b4aed6
Rahullchaudharyy <rahullchaudharryy@gemail.com>
2025-01-05 10:52:34 +0530

    Configure Vite server to run on port 10000 and accept connections from all hosts

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -3,11 +3,10 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  server:{
-    proxy:{
-     '/api/':'https://echonest.onrender.com'
-    }
- },
+  server: {
+    port: 10000,  
+    host: '0.0.0.0' 
+  },
   plugins: [react()],
   build: {
     outDir: 'dist', 
