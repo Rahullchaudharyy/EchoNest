eb3b1c7b4592234e781f5dc39bf6ed48f0e01f3d
Rahullchaudharyy <rahullchaudharryy@gemail.com>
2025-01-14 14:36:28 +0530

    Update axios instance base URL for development environment and clean up commented code

--- src/utils/axiosInstance.js
+++ src/utils/axiosInstance.js
@@ -1,10 +1,18 @@
 import axios from 'axios'
 
 // for the development
+
+
 const axiosInstence = axios.create({
-    baseURL: 'http://localhost:3000',
+    baseURL: '/api',
     withCredentials:true
 })
+
+
+// const axiosInstence = axios.create({
+//     baseURL: 'http://localhost:3000',
+//     withCredentials:true
+// })
 // for the production 
 // const axiosInstence = axios.create({
 //     baseURL:"https://echonest.onrender.com",
